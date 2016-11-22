7a0eade710986783a8c352a0fd6552f595739c3f
Joshua Schipull <joshuaschipull@joshuas-mbp>
2016-11-21 21:03:34 -0600

    Fixed Var warnings

--- MapPin/ViewController.swift
+++ MapPin/ViewController.swift
@@ -24,27 +24,27 @@ class ViewController: UIViewController, MKMapViewDelegate {
         mapView.setRegion(coordinateRegion, animated: false)
         mapView.regionThatFits(coordinateRegion)
         
-        var annotation1 = MKPointAnnotation()
+        let annotation1 = MKPointAnnotation()
         annotation1.title = "Swansea Bay"
         annotation1.subtitle = "Beautiful Beaches"
         annotation1.coordinate = CLLocationCoordinate2DMake(51.587736,-3.90152)
         
-        var annotation2 = MKPointAnnotation()
+        let annotation2 = MKPointAnnotation()
         annotation2.title = "Menai Bridge"
         annotation2.subtitle = "Fantastic Engineering"
         annotation2.coordinate = CLLocationCoordinate2DMake(53.220527,-4.163561)
         
-        var annotation3 = MKPointAnnotation()
+        let annotation3 = MKPointAnnotation()
         annotation3.title = "Parc Y Scarlets"
         annotation3.subtitle = "Oh Dear"
         annotation3.coordinate = CLLocationCoordinate2DMake(51.678809,-4.127469)
         
-        var annotation4 = MKPointAnnotation()
+        let annotation4 = MKPointAnnotation()
         annotation4.title = "Castell Coch"
         annotation4.subtitle = "A Fairytale Castle"
         annotation4.coordinate = CLLocationCoordinate2DMake(51.535819,-3.2547)
         
-        var annotation5 = MKPointAnnotation()
+        let annotation5 = MKPointAnnotation()
         annotation5.title = "Arthur's Stone"
         annotation5.subtitle = "Rock Of Legend"
         annotation5.coordinate = CLLocationCoordinate2DMake(51.593735,-4.179525)
